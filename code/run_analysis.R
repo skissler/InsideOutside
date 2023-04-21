4a792391ca90fad5952e8f9c940ed52ecaf54691
Stephen Kissler <skissler@hsph.harvard.edu>
2023-04-21 17:10:42 -0400

    update

--- code/run_analysis.R
+++ code/run_analysis.R
@@ -6,7 +6,7 @@ library(tidyverse)
 source('code/utils.R')
 
 R0 <- 2.5
-Nint <- 100 
+Nint <- 100
 cint <- 10
 cext <- 10 
 pext <- 0.01
@@ -30,7 +30,7 @@ Avals <- unlist(lapply(-inftimes, A, R0=R0, mean=Amean, nexp=Anexp))
 
 lambdaint <- k/Nint*sum(Avals, na.rm=TRUE)
 
-lambdaext <- (1-k)*pext*qA(0.99, mean=Amean, nexp=Anexp)/R0
+lambdaext <- (1-k)*pext*R0/qA(0.99, mean=Amean, nexp=Anexp)
 
 pint <- lambdaint / (lambdaint + lambdaext)
 
@@ -56,6 +56,18 @@ pintsig <- psigint*pint / (psigint*pint + psigext*(1-pint))
 # =============================================================================
 
 xrng <- seq(from=0, to=10, by=0.01) 
+fig_A <- tibble(x=xrng, y=A(xrng,R0,mean=Amean,nexp=Anexp)) %>% 
+	ggplot() + 
+		geom_line(aes(x=x,y=y),size=0.8) + 
+		annotate("rect", xmin=0, xmax=qA(0.99, mean=Amean, nexp=Anexp), ymin=0, ymax=R0/qA(0.99, mean=Amean, nexp=Anexp), alpha=0.4) + 
+		geom_segment(aes(x=0, xend=qA(0.99, mean=Amean, nexp=Anexp), y=R0/qA(0.99, mean=Amean, nexp=Anexp), yend=R0/qA(0.99, mean=Amean, nexp=Anexp)), lty="dashed") + 
+		labs(x="Time since infection onset", y="Infectiousness") +
+		theme_classic() + 
+		theme(text=element_text(size=9)) 
+ggsave(fig_A, file="figures/A.pdf",width=3.5,height=3.5/1.6,units="in")
+
+
+xrng <- seq(from=0, to=10, by=0.01) 
 sdlabs <- tibble(x=inftimes+2, y=1.02, label=snpdists) %>% 
 	filter(!is.na(x)) %>% 
 	mutate(label=as.character(label))
@@ -71,3 +83,14 @@ fig_Aparade <- as.list(inftimes[!is.na(inftimes)]) %>%
 		labs(x="Time since onset of the focal infection", y="Infectiousness") +
 		theme(text=element_text(size=9))
 ggsave(fig_Aparade, file="figures/Aparade.pdf",width=3.5,height=3.5/1.6,units="in")
+
+fig_xi <- tibble(y=xi/sum(xi)) %>% 
+	mutate(x=1:n()) %>% 
+	mutate(x=x-1) %>% 
+	ggplot(aes(x=x,y=y)) + 
+		geom_col(fill="white",col="black", size=0.8) + 
+		scale_x_continuous(breaks=0:100) + 
+		theme_classic() + 
+		labs(x="SNP distance",y="Proportion of sequences") + 
+		theme(text=element_text(size=9))
+ggsave(fig_xi, file="figures/xi.pdf",width=3.5,height=3.5/1.6,units="in")
