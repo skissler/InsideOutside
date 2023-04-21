7dcfafd6d3f716e485166e04d567b11e29690604
Stephen Kissler <skissler@hsph.harvard.edu>
2023-04-21 16:12:16 -0400

    update

--- code/run_analysis.R
+++ code/run_analysis.R
@@ -1,3 +1,7 @@
+# =============================================================================
+# Import and define 
+# =============================================================================
+
 library(tidyverse) 
 source('code/utils.R')
 
@@ -11,15 +15,18 @@ mu <- 0.01
 Amean <- 2.5
 Anexp <- 5
 
-k <- cint/(cint+cext) 
-
-
 inftimes <- c(-2, -2.5, -3, -4, -6, -12, -20, -25, rep(NA, Nint-8))
 snpdists <- c(0, 1, 1, 2, 3, 3, 2, 5, rep(NA, Nint-8))
 
 xi <- c(20,30,50,25,12,4,2,1)
 
-Avals <- unlist(lapply(-inftimes, A, R0=R0, mean=2.5, nexp=5))
+# =============================================================================
+# Calculate probability of internal transmission
+# =============================================================================
+
+k <- cint/(cint+cext) 
+
+Avals <- unlist(lapply(-inftimes, A, R0=R0, mean=Amean, nexp=Anexp))
 
 lambdaint <- k/Nint*sum(Avals, na.rm=TRUE)
 
@@ -44,27 +51,23 @@ psigext <- getpsigext(xi,mu)
 
 pintsig <- psigint*pint / (psigint*pint + psigext*(1-pint))
 
+# =============================================================================
+# Plot
+# =============================================================================
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-xrng <- seq(from=0,to=10,by=0.01)
-tibble(x=xrng, y=unlist(lapply(xrng, A, mean=Amean, nexp=Anexp))) %>% 
-	ggplot(aes(x=x, y=y)) + 
-		geom_line() + 
-		theme_classic() 
+xrng <- seq(from=0, to=10, by=0.01) 
+sdlabs <- tibble(x=inftimes+2, y=1.02, label=snpdists) %>% 
+	filter(!is.na(x)) %>% 
+	mutate(label=as.character(label))
+fig_Aparade <- as.list(inftimes[!is.na(inftimes)]) %>% 
+	imap(~ tibble(id=.y, x=xrng+.x, y=A(xrng,R0,mean=Amean,nexp=Anexp))) %>% 
+	bind_rows() %>% 
+	ggplot() + 
+		geom_line(aes(x=x,y=y,group=factor(id)), size=0.8, alpha=0.4) + 
+		geom_vline(aes(xintercept=0)) + 
+		geom_hline(aes(yintercept=0)) + 
+		geom_text(data=sdlabs, aes(x=x,y=y,label=label), size=2.5) + 
+		theme_classic() + 
+		labs(x="Time since onset of the focal infection", y="Infectiousness") +
+		theme(text=element_text(size=9))
+ggsave(fig_Aparade, file="figures/Aparade.pdf",width=3.5,height=3.5/1.6,units="in")
